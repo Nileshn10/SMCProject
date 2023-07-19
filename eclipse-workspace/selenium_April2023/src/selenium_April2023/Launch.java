305fdcff95dcb554fcc007686bceb58ce0dafb88
Nileshn10 <nileshnkokate10@gmail.com>
2023-07-19 20:51:18 +0530

    [first in july]Nilesh

---
+++ eclipse-workspace/selenium_April2023/src/selenium_April2023/Launch.java
@@ -0,0 +1,23 @@
+package selenium_April2023;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Launch {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofHours(10));
+		driver.get("https://facebook.om");
+		driver.manage().window().maximize();
+		
+		driver.quit();
+		
+	}
+
+}
---
+++ eclipse-workspace/selenium_April2023/src/selenium_April2023/Screenssssssss.java
@@ -0,0 +1,20 @@
+package selenium_April2023;
+
+public class Screenssssssss {
+	
+	public static void main(String[] args) {
+		
+		String a="Mahadev";
+		String b="";
+		for(int i=a.length()-1;i>=0;i--) {
+			b+=a.charAt(i);
+			
+			
+		}
+		System.out.println("Before reversing "+a);
+		System.out.println(b);
+		
+	}
+	
+}
+	
